32b037ea5810f61a7645aa4ccb10ee5eb4f8cfea
AleAleAnd <alexande.alexeev@gmail.com>
2025-09-19 16:22:50 +0300

    Chart created

--- src/components/layout/AppContent/AppContent.jsx
+++ src/components/layout/AppContent/AppContent.jsx
@@ -1,13 +1,38 @@
-import Layout from "antd/es/layout/layout"
+import Layout from 'antd/es/layout/layout'
+import Typography from 'antd/es/typography/Typography'
+import { useCrypto } from '../../../context/crypto-context'
+import PortfolioChart from '../PortfolioChart'
+import AssetsTable from '../AssetsTable'
 
 const contentStyle = {
   textAlign: 'center',
   minHeight: 'calc(100vh - 60px)',
   color: '#fff',
   backgroundColor: '#001529',
-  padding: '1rem'
+  padding: '1rem',
 }
 
-export default function AppContent(){
-    return <Layout.Content style={contentStyle}>Content</Layout.Content>
+export default function AppContent() {
+  const { assets, crypto } = useCrypto()
+
+  const cryptoPriceMap = crypto.reduce((acc, c) => {
+    acc[c.id] = c.price
+    return acc
+  }, {})
+
+  return (
+    <Layout.Content style={contentStyle}>
+      <Typography.Title level={3} style={{ textAlign: 'left', color: '#fff' }}>
+        Portfolio:{' '}
+        {assets
+          .map((asset) => asset.amount * cryptoPriceMap[asset.id])
+          .reduce((acc, v) => (acc += v), 0)
+          .toFixed(2)}
+        $
+      </Typography.Title>
+
+      <PortfolioChart></PortfolioChart>
+      <AssetsTable></AssetsTable>
+    </Layout.Content>
+  )
 }
